6ddd6dd962770e54f4bb70d64b2bef867e626196
Eugen <singularis314@gmail.com>
2025-06-26 23:41:28 +0200

    Update versionCode and versionName in build.gradle.kts to reflect the new app version 2.2 (4).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.singularis.eateria"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         

f81dce8b9306e66607b264732030ebcbe82a245e
Eugen <singularis314@gmail.com>
2025-07-06 11:27:10 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.singularis.eateria"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "2.5"
+        versionCode = 8
+        versionName = "2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         

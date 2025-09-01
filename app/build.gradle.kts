c2a0c78c5a915df304510c711efd0150e49f5af6
Eugen Parasochka <singularis314@gmail.com>
2025-09-01 21:41:10 +0200

    Update versioning to reflect new release. Bump versionCode to 12 and versionName to "2.91" in build.gradle.kts, preparing for the next app update.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.singularis.eateria"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "2.9"
+        versionCode = 12
+        versionName = "2.91"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
